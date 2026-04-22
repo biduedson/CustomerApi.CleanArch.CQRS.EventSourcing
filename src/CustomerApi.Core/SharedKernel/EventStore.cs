91cb8661ce106570da16c8ff7d0d4a0df7f297ba
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-22 17:19:23 -0300

    feat: add eventstore and interface  eventstore async

---
+++ src/CustomerApi.Core/SharedKernel/EventStore.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace CustomerApi.Core.SharedKernel;
+
+public class EventStore : BaseEvent
+{
+
+    public EventStore(Guid aggregateId, string messageType, string data)
+    {
+        AggregateId = aggregateId;
+        MessageType = messageType;
+        Data = data;
+    }
+    public EventStore() { }
+
+    public Guid Id { get; private init; } = Guid.NewGuid();
+    public string Data { get; private init; }
+}
---
+++ src/CustomerApi.Core/SharedKernel/IEventStoreRepository.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace CustomerApi.Core.SharedKernel;
+
+public interface IEventStoreRepository : IDisposable
+{
+    Task StoreAsync(IEnumerable<EventStore> eventStores);
+}
