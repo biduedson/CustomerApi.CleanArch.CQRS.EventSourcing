1c503d0ace0e6b3cfd424ece5a8eee0716e5b523
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-22 18:59:35 -0300

    feat(core): add  assembly extensions

---
+++ src/CustomerApi.Core/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.Reflection;
+
+namespace CustomerApi.Core.Extensions;
+
+public static class AssemblyExtensions
+{
+    public static IEnumerable<Type> GetAllTypesOf<TInterface>(this Assembly assembly)
+    {
+        var isAssignaBleToInterface = typeof(TInterface).IsAssignableFrom;
+        return [.. assembly
+            .GetTypes()
+            .Where(type => type.IsClass && !type.IsAbstract && !type.IsInterface && isAssignaBleToInterface(type))];
+    }
+}
