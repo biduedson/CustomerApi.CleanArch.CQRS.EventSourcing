daba42e33958befeb0e330dfc1241855bb27fad2
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-22 18:11:34 -0300

    feat: add extensions appsetings of iconfiguration and iserviceprovider

---
+++ src/CustomerApi.Core/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+
+using CustomerApi.Core.SharedKernel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace CustomerApi.Core.Extensions;
+
+public static class ServiceProviderExtensions
+{
+    public static TOptions GetOptions<TOptions>(this IServiceProvider serviceProvider)
+        where TOptions : class, IAppOptions =>
+        serviceProvider.GetService<IOptions<TOptions>>()?.Value;
+}
