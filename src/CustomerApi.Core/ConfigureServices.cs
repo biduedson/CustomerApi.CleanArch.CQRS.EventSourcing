a03307028eb35a01ec9853cc86de7be1e3c5eb9a
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-22 19:38:58 -0300

    feat(core):add jsonextension and genererictypes extensions and configureservices

--- src/CustomerApi.Core/ConfigureServices.cs
+++ src/CustomerApi.Core/ConfigureServices.cs
@@ -1,6 +1,24 @@
-﻿namespace CustomerApi.Core;
+﻿using CustomerApi.Core.AppSettings;
+using CustomerApi.Core.SharedKernel;
+using Microsoft.Extensions.DependencyInjection;
 
-public class ConfigureServices
+namespace CustomerApi.Core;
+
+public static class ConfigureServices
 {
+    public static IServiceCollection ConfigureAppSettings(this IServiceCollection services) =>
+            services
+            .AddOptionsWithValidation<ConnectionOptions>()
+            .AddOptionsWithValidation<CacheOptions>();
 
+    private static IServiceCollection AddOptionsWithValidation<TOptions>(this IServiceCollection services)
+        where  TOptions : class, IAppOptions
+    {
+        return services
+            .AddOptions<TOptions>()
+            .BindConfiguration(TOptions.ConfigureSectionPath, binderOptions => binderOptions.BindNonPublicProperties = true)
+            .ValidateDataAnnotations()
+            .ValidateOnStart()
+            .Services;
+    }
 }
---
+++ src/CustomerApi.Core/Extensions/GenericTypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace CustomerApi.Core.Extensions;
+
+public static class GenericTypeExtensions
+{
+    public static bool IsDefault<T>(this T value) =>
+        Equals(value, default(T));
+
+    public static string GetGenericTypeName(this object @object)
+    {
+        var type = @object.GetType();
+
+        if (!type.IsGenericType)
+            return type.Name;
+
+        var genericTypes = string.Join(",", type.GetGenericArguments().Select(t => t.Name).ToArray());
+        return $"{type.Name[..type.Name.IndexOf('`')]}<{genericTypes}>";
+    }
+}
