2908bfab0c1e9ddbdba4ead06bc7c80351805a51
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-22 17:49:56 -0300

    feat: add appsettings  config core

---
+++ src/CustomerApi.Core/AppSettings/ConnectionOptions.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using CustomerApi.Core.SharedKernel;
+
+namespace CustomerApi.Core.AppSettings;
+public sealed class ConnectionOptions : IAppOptions
+{
+    static string IAppOptions.ConfigureSectionPath => "ConnectionStrings";
+
+    [Required]
+    public string SqlConnection { get; private init; }
+
+    [Required]
+    public string NoSqlConnection { get; private init; }
+
+    [Required]
+    public string  CacheConnection { get; private init; }
+
+    public bool CacheConnectionInMemory() =>
+        CacheConnection.Equals("InMemory", StringComparison.InvariantCultureIgnoreCase);
+}
