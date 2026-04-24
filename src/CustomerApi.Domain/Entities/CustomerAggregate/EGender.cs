c9132527285fc07a84fbfcc0b1cc6a01cff0b41f
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-24 15:46:00 -0300

    feat: add egender enum

---
+++ src/CustomerApi.Domain/Entities/CustomerAggregate/EGender.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace CustomerApi.Domain.Entities.CustomerAggregate;
+
+[JsonConverter(typeof(JsonStringEnumConverter<EGender>))]
+public enum  EGender
+{
+    None = 0,
+    Male = 1,
+    Female = 2
+}
