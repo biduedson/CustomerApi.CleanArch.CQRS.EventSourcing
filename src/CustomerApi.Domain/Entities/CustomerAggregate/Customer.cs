4e5616059cdf4534d67fbd023a51b01fb6402924
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-24 16:26:56 -0300

    feat: add customer entity aggregate

---
+++ src/CustomerApi.Domain/Entities/CustomerAggregate/Customer.cs
@@ -0,0 +1,52 @@
+﻿
+
+using CustomerApi.Core.SharedKernel;
+using CustomerApi.Domain.Entities.CustomerAggregate.Events;
+using CustomerApi.Domain.ValueObjects;
+
+namespace CustomerApi.Domain.Entities.CustomerAggregate;
+
+public class Customer : BaseEntity, IAggregateRoot
+{
+    private bool _isDeleted;
+
+
+    public Customer(string firstName, string lastName, EGender gender, Email email, DateTime dateOfBirth) 
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Gender = gender;
+        Email = email;
+        DateOfBirth = dateOfBirth;
+
+        AddDomainEvents(new CustomerCreatedEvent(Id, firstName, lastName, gender, email.Address, dateOfBirth));
+    }
+
+    public Customer() { }
+
+
+    public string FirstName { get;}
+    public string LastName { get;}
+    public EGender Gender { get; }
+    public Email Email { get; private set; }
+    public DateTime DateOfBirth { get; }
+
+    public void ChangeEmail(Email newEmail)
+    {
+        if (Email.Equals(newEmail))
+            return;
+
+       Email = newEmail;
+       
+        AddDomainEvents(new CustomerUpdatedEvent(Id, FirstName, LastName, Gender, Email.Address, DateOfBirth));
+    }
+
+    public  void Delete()
+    {
+        if (_isDeleted)
+            return;
+        _isDeleted = true;
+
+        AddDomainEvents(new CustomerUpdatedEvent(Id, FirstName, LastName, Gender, Email.Address, DateOfBirth));
+    }
+}
