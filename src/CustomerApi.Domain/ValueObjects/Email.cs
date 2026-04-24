5f049e045affbfb5bd764eaf5cf976b966153683
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-24 16:01:37 -0300

    feat: add value object email

---
+++ src/CustomerApi.Domain/ValueObjects/Email.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Ardalis.Result;
+using CustomerApi.Core;
+
+namespace CustomerApi.Domain.ValueObjects;
+
+public sealed record  Email
+{
+    private Email(string address) =>
+        Address = address.ToLowerInvariant().Trim();
+
+    public Email() { }
+    public string Address { get; }
+
+    public static Result<Email> Create(string emailAddress)
+    {
+        if (string.IsNullOrWhiteSpace(emailAddress))
+            return Result<Email>.Error("O endereço de e-mail deve ser fornecido.");
+
+        return !RegexPatterns.EmailIsValid.IsMatch(emailAddress)
+            ? Result<Email>.Error("O endereço de e-mail é inválido.")
+            : Result<Email>.Success(new Email(emailAddress));
+    }
+}
