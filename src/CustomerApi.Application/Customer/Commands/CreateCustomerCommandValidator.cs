4802a69e8dc29fe0a3c775ff6fe0fa3146527590
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-25 18:29:18 -0300

    feat: add create customer  commands and  handlers

---
+++ src/CustomerApi.Application/Customer/Commands/CreateCustomerCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿
+using FluentValidation;
+
+namespace CustomerApi.Application.Customer.Commands;
+
+public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+{
+    public CreateCustomerCommandValidator()
+    {
+        RuleFor(command => command.FirstName)
+            .NotEmpty()
+            .MaximumLength(100);
+        RuleFor(command => command.LastName)
+            .NotEmpty()
+            .MaximumLength(100);
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(254);
+        RuleFor(command => command.DateOfBirth)
+            .NotEmpty()
+            .LessThan(DateTime.Today);
+    }
+}
---
+++ src/CustomerApi.Application/Customer/Responses/CreatedCustomerResponse.cs
@@ -0,0 +1,10 @@
+﻿
+
+using CustomerApi.Core.SharedKernel;
+
+namespace CustomerApi.Application.Customer.Responses;
+
+public  class CreatedCustomerResponse(Guid Id) : IResponse
+{
+    public Guid Id { get; } = Id;
+}
