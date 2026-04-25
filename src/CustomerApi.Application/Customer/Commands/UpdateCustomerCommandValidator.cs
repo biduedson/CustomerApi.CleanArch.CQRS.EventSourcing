7db287fe95e4d5ce31cdf69c2063281fb7de5104
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-25 19:43:10 -0300

    feat: add update customer comand an handler

---
+++ src/CustomerApi.Application/Customer/Commands/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿
+
+using FluentValidation;
+
+namespace CustomerApi.Application.Customer.Commands;
+
+public class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+{
+    public UpdateCustomerCommandValidator()
+    {
+            RuleFor(command => command.Id)
+                .NotEmpty();
+            RuleFor(command => command.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(254);
+    }
+}
