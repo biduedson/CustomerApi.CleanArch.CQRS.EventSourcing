2e6fd2d9a7f660eb005ec693a44e01bae696f0ce
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-29 15:24:08 -0300

    feat: add customer map

---
+++ src/CustomerApi.Query/Data/Context/Mappings/CustomerMap.cs
@@ -0,0 +1,40 @@
+﻿using CustomerApi.Query.Abstractions;
+using CustomerApi.Query.QueriesModel;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace CustomerApi.Query.Data.Context.Mappings;
+
+public class CustomerMap : IReadDbMapping
+{
+    public void Configure()
+    {
+        BsonClassMap.TryRegisterClassMap<CustomerQueryModel>(classMap =>
+        {
+            classMap.AutoMap();
+            classMap.SetIgnoreExtraElements(true);
+
+            classMap.MapMember(customer => customer.Id)
+            .SetIsRequired(true);
+
+            classMap.MapMember(customer => customer.FirstName)
+               .SetIsRequired(true);
+
+            classMap.MapMember(customer => customer.LastName)
+                .SetIsRequired(true);
+
+            classMap.MapMember(customer => customer.Gender)
+                .SetIsRequired(true);
+
+            classMap.MapMember(customer => customer.Email)
+                .SetIsRequired(true);
+
+            classMap.MapMember(customer => customer.DateOfBirth)
+                .SetIsRequired(true)
+                .SetSerializer(new DateTimeSerializer(true));
+
+             classMap.UnmapMember(customer => customer.FullName);
+        });
+
+    }
+}
