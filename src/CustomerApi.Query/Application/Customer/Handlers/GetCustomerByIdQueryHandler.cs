023384e93376e859840faad30aac4325c595b627
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-29 16:22:03 -0300

    feat > add queries and queries handlers

---
+++ src/CustomerApi.Query/Application/Customer/Handlers/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.Result;
+using Ardalis.Result.FluentValidation;
+using CustomerApi.Core.SharedKernel;
+using CustomerApi.Query.Application.Customer.Queries;
+using CustomerApi.Query.Data.Repositories.Abstractions;
+using CustomerApi.Query.QueriesModel;
+using FluentValidation;
+using MediatR;
+
+namespace CustomerApi.Query.Application.Customer.Handlers;
+
+public class GetCustomerByIdQueryHandler(
+    IValidator<GetCustomerByIdQuery> validator,
+    ICustomerReadOnlyRepository repository,
+    ICacheService cacheService
+    ) : IRequestHandler<GetCustomerByIdQuery, Result<CustomerQueryModel>>
+{
+    public async Task<Result<CustomerQueryModel>> Handle(
+        GetCustomerByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid)
+            return Result<CustomerQueryModel>.Invalid(validationResult.AsErrors());
+
+        var cacheKey = $"{nameof(GetCustomerByIdQuery)}_{request.Id}";
+
+        var customer = await cacheService.GetOrCreateAsync(cacheKey, () => repository.GetByIdAsync(request.Id));
+
+        return customer == null
+            ? Result<CustomerQueryModel>.NotFound($"Nenhum cliente encontrado com o Id: {request.Id}")
+            : Result<CustomerQueryModel>.Success(customer);
+    }
+}
