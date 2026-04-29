42d6da04aba5fc96df9d09c45f0c4e14f140bda5
Edson Gomes <biduzao.bidu21@gmail.com>
2026-04-29 15:46:26 -0300

    feat: repositories setup

---
+++ src/CustomerApi.Query/Data/Context/Repositories/Abstractions/CustomerReadOnlyRepository.cs
@@ -0,0 +1,25 @@
+﻿using CustomerApi.Query.Abstractions;
+using CustomerApi.Query.QueriesModel;
+using MongoDB.Driver;
+
+namespace CustomerApi.Query.Data.Context.Repositories.Abstractions;
+
+internal class CustomerReadOnlyRepository(IReadDbContext readDbContext)
+     : BaseReadOnlyRepository<CustomerQueryModel, Guid>(readDbContext), ICustomerReadOnlyRepository
+{
+    public async Task<IEnumerable<CustomerQueryModel>> GetAllAsync()
+    {
+        var sort = Builders<CustomerQueryModel>.Sort
+            .Ascending(customer => customer.FirstName)
+            .Descending(customer => customer.DateOfBirth);
+
+        var findOptions = new FindOptions<CustomerQueryModel>
+        {
+            Sort = sort
+        };
+
+        using var asyncCursor = await Collection.FindAsync(Builders<CustomerQueryModel>.Filter.Empty, findOptions);
+        return await asyncCursor.ToListAsync();
+    }
+
+}
---
+++ src/CustomerApi.Query/Data/Context/Repositories/BaseReadOnlyRepository.cs
@@ -0,0 +1,17 @@
+﻿using CustomerApi.Query.Abstractions;
+using MongoDB.Driver;
+
+namespace CustomerApi.Query.Data.Context.Repositories;
+
+internal abstract class BaseReadOnlyRepository<TQueryModel, TKey>(IReadDbContext context) : IReadOnlyRepository<TQueryModel, TKey>
+    where TQueryModel : IQueryModel<TKey>
+    where  TKey : IEquatable<TKey>
+{
+    protected readonly IMongoCollection<TQueryModel> Collection = context.GetCollection<TQueryModel>();
+
+    public async Task<TQueryModel> GetByIdAsync(TKey id)
+    {
+        using var asyncCusrsor = await Collection.FindAsync(queryModel => queryModel.Id.Equals(id));
+        return await asyncCusrsor.FirstOrDefaultAsync();
+    }
+}
