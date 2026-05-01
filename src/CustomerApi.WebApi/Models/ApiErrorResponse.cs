0ffaecbfd71ec4ef02eb048e618ade690fdc134c
Edson Gomes <biduzao.bidu21@gmail.com>
2026-05-01 14:41:40 -0300

    feat: add api respondes models

---
+++ src/CustomerApi.WebApi/Models/ApiErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CustomerApi.WebApi.Models;
+
+[method: JsonConstructor]
+public sealed class ApiErrorResponse(string message)
+{
+    public string Message { get; } = message;
+
+    public override string ToString() => Message;
+
+}
--- src/CustomerApi.WebApi/Program.cs
+++ src/CustomerApi.WebApi/Program.cs
@@ -1,34 +1,19 @@
 
-namespace CustomerApi.WebApi
-{
-    public class Program
+using System.IO.Compression;
+using CustomerApi.Core.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .Configure<GzipCompressionProviderOptions>(compressionOptions => compressionOptions.Level = CompressionLevel.Fastest)
+    .Configure<JsonOptions>(jsonOption => jsonOption.JsonSerializerOptions.Configure())
+    .Configure<RouteOptions>(routeOptions => routeOptions.LowercaseUrls = true)
+    .AddHttpClient()
+    .AddHttpContextAccessor()
+    .AddResponseCompression(compressionOptions =>
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
-}
+        compressionOptions.EnableForHttps = true;
+        compressionOptions.Providers.Add<GzipCompressionProvider>();
+    });
